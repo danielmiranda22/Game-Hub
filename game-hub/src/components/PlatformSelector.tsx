c858c1cb5a89b934199c9d5edafa9ec5580c4f1d
Daniel Oliveira <daniel.oliveira@innux.com>
2024-07-06 17:09:03 +0100

    Apply react-query to fetch the platforms.

--- game-hub/src/components/PlatformSelector.tsx
+++ game-hub/src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((p) => (
+        {data.results.map((p) => (
           <MenuItem onClick={() => onSelectedPlatform(p)} key={p.id}>
             {p.name}
           </MenuItem>
