533e07bbb1b53e06078b92106fa092b3792d1ae1
Daniel Oliveira <daniel.oliveira@innux.com>
2024-07-24 21:47:49 +0100

    Build game trailer

--- game-hub/src/pages/GameDetailPage.tsx
+++ game-hub/src/pages/GameDetailPage.tsx
@@ -2,11 +2,11 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAtributes from "../components/GameAtributes";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
@@ -18,6 +18,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAtributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
