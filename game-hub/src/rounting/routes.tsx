485d304079a0c877cdd6dc8ed9b62290bb842c09
Daniel Oliveira <daniel.oliveira@innux.com>
2024-07-24 15:41:22 +0100

    Handle errors.

--- game-hub/src/rounting/routes.tsx
+++ game-hub/src/rounting/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErroPage from "../pages/ErroPage";
 import GameDetailPage from "../pages/GameDetailPage";
 import HomePage from "../pages/HomePage";
 import Layout from "../pages/Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErroPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "gameDetail/:id", element: <GameDetailPage /> },
