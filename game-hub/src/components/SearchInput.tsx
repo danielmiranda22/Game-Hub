e08f54ef1f4607dc963b224e876052c3fbe7e399
Daniel Oliveira <daniel.oliveira@innux.com>
2024-07-24 22:20:39 +0100

    Fix the navbar

--- game-hub/src/components/SearchInput.tsx
+++ game-hub/src/components/SearchInput.tsx
@@ -1,19 +1,23 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
-
   // component is only dependent on this function. Even if there is another thing that changes, this component dosen't rerender
   const setSearchtText = useGameQueryStore((s) => s.setSearchtText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        if (ref.current) setSearchtText(ref.current.value);
+        if (ref.current) {
+          setSearchtText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
